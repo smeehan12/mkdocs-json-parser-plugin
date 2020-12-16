884014f9ba15bb8ed44fc593f74cde6683226083
Samuel Meehan <smeehan12@gmail.com>
2020-12-16 12:12:49 -0500

    mod

--- mkdocsjson/plugin.py
+++ mkdocsjson/plugin.py
@@ -8,27 +8,24 @@ from mkdocs.config import config_options as mkd
 from .configitems import ConfigItems
 from mkdocs.plugins import BasePlugin
 
-def runParser(basedir, cfg=None, workdir=None, dest=None, tryClone=False, recursive=False):
-    logger.info("Running json parse for {0} with {1}, saving into {2}".format(basedir,cfg,dest))
 
 class JsonPlugin(BasePlugin):
     config_scheme = (
-        ("packages", ConfigItems(
-            ("url"    , mkd.Type(str)),
-            ("config" , mkd.Type(str)),
-            ("workdir", mkd.Type(str)),
-            )),
-        ("tryclone", mkd.Type(bool, default=False)))
+        "url"    ,  mkd.Type(str),
+        "schemas" , mkd.Type(str)
+        )
 
     def on_post_build(self, config):
-        for pkgConf in self.config["packages"]:
-            for outname, cfg in pkgConf.items():
-                outpath = os.path.abspath(os.path.join(config["site_dir"], outname))
-                try:
-                    basedir = cfg.get("url", ".")
-                    icfg = cfg.get("config")
-                    logger.info("Running json parse for {0} with {1}, saving into {2}".format(
-                        (basedir if basedir != "." else "current directory"), (icfg if icfg else "default config"), outpath))
-                    runParser(basedir, cfg=icfg, workdir=cfg.get("workdir"), dest=outpath, tryClone=self.config["tryclone"])                
-                except Exception as e:
-                    logger.error("Skipped doxygen for package {0}: {1!s}".format(outname, e))
+    
+        url     = self.config["url"]
+        schemas = self.config["schemas"]
+        
+        logger.info("Running json parse for {0}".format(url))
+        for schema in schemas:
+          logger.info("Parsing schema : ".format(schema))
+          
+        outpath = os.path.abspath(os.path.join(config["site_dir"], schema.split('.')[0], ".md"))
+        
+        logger.info("Outpath : ".format(outpath))
+    
+
