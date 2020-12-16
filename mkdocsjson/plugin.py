0138936c5f6b83731887240a8f4f6be6d31e2759
Samuel Meehan <smeehan12@gmail.com>
2020-12-16 12:38:17 -0500

    mod

--- mkdocsjson/plugin.py
+++ mkdocsjson/plugin.py
@@ -11,21 +11,24 @@ from mkdocs.plugins import BasePlugin
 
 class JsonPlugin(BasePlugin):
     config_scheme = (
-        "url"    ,  mkd.Type(str),
-        "schemas" , mkd.Type(str)
+        ("packages", ConfigItems(
+            ("url"    , mkd.Type(str)),
+            ("config" , mkd.Type(str)),
+            ("workdir", mkd.Type(str)),
+            )),
+        ("tryclone", mkd.Type(bool, default=False)),
+        ("recursive", mkd.Type(bool, default=False)),
         )
 
     def on_post_build(self, config):
-    
-        url     = self.config["url"]
-        schemas = self.config["schemas"]
-        
-        logger.info("Running json parse for {0}".format(url))
-        for schema in schemas:
-          logger.info("Parsing schema : ".format(schema))
-          
-        outpath = os.path.abspath(os.path.join(config["site_dir"], schema.split('.')[0], ".md"))
-        
-        logger.info("Outpath : ".format(outpath))
-    
-
+        for pkgConf in self.config["packages"]:
+            for outname, cfg in pkgConf.items():
+                outpath = os.path.abspath(os.path.join(config["site_dir"], outname))
+                try:
+                    basedir = cfg.get("url", ".")
+                    icfg = cfg.get("config")
+                    logger.info("Running doxygen for {0} with {1}, saving into {2}".format(
+                        (basedir if basedir != "." else "current directory"), (icfg if icfg else "default config"), outpath))
+                    runDoxygen(basedir, cfg=icfg, workdir=cfg.get("workdir"), dest=outpath, tryClone=self.config["tryclone"], recursive=self.config["recursive"])                
+                except Exception as e:
+                    logger.error("Skipped doxygen for package {0}: {1!s}".format(outname, e))
